bdab343c57ed97632d0dfce7ad285f222b3ae852
canin <canin@canin-server.yak.org>
2018-04-14 23:47:43 -0500

    new bash script to update events and bills

---
+++ scripts/bash/update_bills.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source /var/www/mn.actibase/bin/activate
+
+cd /var/www/mn.actibase/actibase/scripts/state
+pupa update Minnesota bills 
+
+deactivate
---
+++ scripts/bash/update_state_full.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source /var/www/mn.actibase/bin/activate
+
+cd /var/www/mn.actibase/actibase/scripts/state
+pupa update Minnesota house senate comms bills
+
+
+deactivate
